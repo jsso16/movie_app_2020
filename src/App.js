762040195f2fe8512acabb8bfe1821ed89f31440
jsso16 <wjsthwls24@daum.net>
2020-09-18 17:03:53 +0900

    Edit App.js App.js 파일을 수정함(컴포넌트 실습)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
+function Food({fav}) {
+  return (
+  <h3>I love {fav}</h3>
+  );
+}
+
+const foodLink = [
+  {
+    name: '김치'
+  },
+  {
+    name: '라면'
+  },
+  {
+    name: '삽겹살'
+  }
+];
+
 function App() {
- // return <div className="App" />;
- return <div>Hello!!!!!</div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;

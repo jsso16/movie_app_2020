7cbbfb3c2df168088bf1f78a6636bbaa72b3c1ab
jsso16 <wjsthwls24@daum.net>
2020-10-01 01:44:33 +0900

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
 import React from 'react';
+import img1 from './images/steak.jpg';
+import img2 from './images/pasta.jpg';
+import img3 from './images/pizza.jpg';
+import img4 from './images/cake.jpg';
+import img5 from './images/lemonade.jpg';
 
-function Food({fav}) {
+function Food({name, image, altText}) {
   return (
-  <h3>I love {fav}</h3>
+    <div>
+      <h2>I love {name}</h2>
+      <img src={image} alt={altText} />
+    </div>
   );
 }
 
-const foodLink = [
+const foodILike = [
   {
-    name: '김치'
+    id: 1,
+    name: '스테이크',
+    image: img1,
+    altText: '소고기 스테이크'
+
   },
   {
-    name: '라면'
+    id: 2,
+    name: '파스타',
+    image: img2,
+    altText: '오일 파스타'
   },
   {
-    name: '삽겹살'
+    id: 3,
+    name: '피자',
+    image: img3,
+    altText: '콤비네이션 피자'
+  },
+  {
+    id: 4,
+    name: '케이크',
+    image: img4,
+    altText: '망고 케이크'
+  },
+  {
+    id: 5,
+    name: '에이드',
+    image: img5,
+    altText: '레몬 에이드'
   }
 ];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      {foodILike.map(dish => (<Food key ={dish.id} name={dish.name} image={dish.image} altText={dish.altText} />))}
     </div>
   );
 }

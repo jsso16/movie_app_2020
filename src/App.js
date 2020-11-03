2f1ceb90df5dee6b97a793e7a55f5be94b6b52b8
jsso16 <wjsthwls24@daum.net>
2020-11-03 22:06:20 +0900

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ class App extends React.Component {
       }
     } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
     this.setState({movies, isLoading: false});
-    console.log(movies);
-  }
+  };
 
   componentDidMount() {
     // 영화 데이터 로딩
@@ -28,14 +27,14 @@ class App extends React.Component {
     return (
       <div>
         {isLoading ? 'Loading...' : movies.map((movie) => {
-          return(
+          return (
             <Movie
-            key={movie.id}
-            id={movie.id}
-            year={movie.year}
-            title={movie.title}
-            summary={movie.summary}
-            poster={movie.medium_cover_image}
+              key={movie.id}
+              id={movie.id}
+             year={movie.year}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
             />
           );
         })}

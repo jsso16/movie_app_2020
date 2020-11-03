a138baa413343640d0a604209d9ed588f4914523
jsso16 <wjsthwls24@daum.net>
2020-11-03 22:06:37 +0900

    Edit Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie(id, year, title, summary, poster) {
+function Movie({id, year, title, summary, poster}) { 
     return <h1>{title}</h1>;
 }
 
 Movie.propTypes = {
     id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
-    title: PropTypes.String.isRequired,
-    summary: PropTypes.String.isRequired,
-    poster: PropTypes.String.isRequired
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 };
 
 export default Movie;

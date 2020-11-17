6ddf75cc990d0d174ec67659198230c950b4a7e1
jsso16 <wjsthwls24@daum.net>
2020-11-17 22:18:13 +0900

    Add About files

---
+++ src/routes/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css'
+
+function About() {
+  return (
+    <div className="about-container">
+      <span>
+        201930231 전소진입니다.<br />
+        "Freedom is the freedom to say that two plus two make four. 
+        If that is granted, all else follows."
+      </span>
+      <span>- Gerge Orwell, 1984 & 전소진, 2020.11.13</span>
+    </div>
+  ); 
+}
+
+export default About;

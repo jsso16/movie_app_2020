13f252e392f456be789ecfc51677fb6a260db632
jsso16 <wjsthwls24@daum.net>
2020-11-20 16:28:40 +0900

    Updates

---
+++ precache-manifest.e1c897278b46655f9e2e8ac5aa46a668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808b97e4a62e7a2be7bf425cf9b560fd",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ea12ac650adac1dfed90",
+    "url": "/movie_app_2020/static/css/main.a25801d0.chunk.css"
+  },
+  {
+    "revision": "e2db8dbeff0daf97e999",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea12ac650adac1dfed90",
+    "url": "/movie_app_2020/static/js/main.5e9ad7b5.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
